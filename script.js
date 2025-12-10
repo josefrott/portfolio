7cf355cdb2b938bab1c70426c3e794bfbcaab43d
Josef Rott <josef.rott96@gmail.com>
2025-12-10 08:51:40 +0100

    u

--- script.js
+++ script.js
@@ -345,7 +345,7 @@ PDF Zoom Feature - Ctrl+Scroll in Box
     // Neue Zoom-Level
     const isZoomIn = e.deltaY < 0;
     const zoomStep = 0.15;
-    const minZoom = 0.5;
+    const minZoom = 1;
     const maxZoom = 4;
 
     let newZoom = isZoomIn 
